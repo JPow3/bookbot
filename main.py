d2ae2d0be212b7d9ab093b9c98b6cf050557b9fe
Joe Pow <joepow@ultra.me>
2025-05-17 10:11:33 -0400

    final commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_letters
 from stats import final_sort
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,11 +9,14 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    text = get_book_text(sys.argv[1])
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)    
     print("============ BOOKBOT ============")   
-    print("Analyzing book found at books/frankenstein.txt...")  
+    print(f"Analyzing book found at {sys.argv[1]}...")  
     print("----------- Word Count ----------") 
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
